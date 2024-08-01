0ae84aba48f0759faaf2db15cbe94115a87b1e5b
Bennett Yeagley <46436812+byeagley@users.noreply.github.com>
2024-08-01 14:04:00 -0500

    change get_unread_messages() to return formatted string

--- instagram_emailer/main.py
+++ instagram_emailer/main.py
@@ -2,14 +2,25 @@ from dotenv import dotenv_values
 from instagrapi import Client
 
 
-def get_unread_messages(client: Client):
-    threads = client.direct_threads(selected_filter="unread")
+def get_unread_messages(client: Client) -> str:
+    unread_messages = ""
+
+    threads = client.direct_threads(selected_filter="unread", thread_message_limit=20)
     for thread in threads:
-        sender = thread.inviter.full_name if thread.inviter else thread.thread_title
+        sender = thread.users[0].username if thread.users else thread.thread_title
+        unread_messages += f"Last few messages from {sender}:\n"
+
         for message in thread.messages:
-            if not message.is_sent_by_viewer:
-                print(f"{sender}: {message.text}")
-                print()
+            if message.is_sent_by_viewer:
+                continue
+
+            if message.text:
+                unread_messages += f"{message.text}\n"
+            else:
+                unread_messages += f"[Media: {message.item_type}]\n"
+        unread_messages += "\n\n"
+
+    return unread_messages
 
 
 def main():
@@ -18,7 +29,9 @@ def main():
     client = Client()
     client.login(config["USERNAME"], config["PASSWORD"])
 
-    get_unread_messages(client)
+    unread_messages = get_unread_messages(client)
+    print(unread_messages)
+
 
 
 if __name__ == "__main__":
